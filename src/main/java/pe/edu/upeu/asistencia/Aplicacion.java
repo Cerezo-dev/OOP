370cc1f2e4b87c9015647b4cd50b530e1ec8d4e0
Shunn <unacuentamasuwux@gmail.com>
2025-08-24 16:50:26 +0000

    change main_asistencia && add Asistencia controller

--- src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
+++ src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -1,8 +1,8 @@
 package pe.edu.upeu.asistencia;
 
-public class Aplication {
+public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("Iniciando app DMP");
+        System.out.println("Iniciando Aplicacion DMP");
         AsistenciaApplication.main(args);
     }
 }
