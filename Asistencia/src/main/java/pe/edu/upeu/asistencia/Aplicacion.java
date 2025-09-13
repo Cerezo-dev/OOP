295a8ddecf1a5f93445445569415853ec94a1d37
Cerezo-dev <unacuentamasuwux@gmail.com>
2025-09-12 19:45:44 -0500

    a

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -2,7 +2,7 @@ package pe.edu.upeu.asistencia;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("Iniciando la aplicacion...");
+        System.out.println("Iniciando Aplicacion DMP");
         AsistenciaApplication.main(args);
     }
 }
