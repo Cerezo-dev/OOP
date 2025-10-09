a5eff20a55538cfe68533eaccace0e168838083d
Cerezo-dev <unacuentamasuwus@gmail.com>
2025-10-09 22:07:22 +0000

    del: basicCopcepts OOP & SysVentas

--- BaiscConceptsPOO/src/main/java/pe/edu/upeu/polimorfismoAbstraccion/ClaseMain.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.polimorfismoAbstraccion;
-
-public class ClaseMain {
-    public static void main(String[] args) {
-        Loro loro = new Loro();
-        loro.animalSound();
-        loro.sleep();
-    }
-}
--- BaiscConceptsPOO/src/main/java/pe/edu/upeu/polimorismo/Loro.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.polimorismo;
-
-public class Loro extends Animal{
-
-    @Override
-    public void emitirSonido() {
-        System.out.println("El loro dice: ¡Hola, hola!");
-    }
-}
